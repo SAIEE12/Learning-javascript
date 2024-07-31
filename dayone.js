b1c5acc38c8932216bdd07152047a2ea8a87248f
SAIEE12 <saimanishsai19189@gmail.com>
2024-07-31 20:58:12 +0530

    my  third commit

--- dayone.js
+++ dayone.js
@@ -1,1 +1,13 @@
-console.log("Welcome to my world")
+//console.log("Welcome to my world");
+//variables
+myName = "saimanish";
+price = 77;
+isFollow = true;
+console.log(isFollow);
+
+
+// var , let , const 
+
+// datatypes --> primitives , non-primitives
+//primitives - number,string,boolean,undefined,null,bigint,symbol
+//non-primitives - Objects (Arrays,functions) 
