f420a90140d91235689f2636b5b0b464ce855241
Your Name <your.email@example.com>
2024-08-01 22:06:06 +0530

    day-3 first commit

---
+++ day3.js
@@ -0,0 +1,16 @@
+// a = 3;
+// b = "3";
+// console.log(a !== b);
+
+let mode = "dark";
+let color;
+
+if (mode === "dark"){
+    color = "black"
+}
+if(mode === "light"){
+    color = "White"
+}
+console.log(color);
+
+alert("welcome to my world..."); //one tme popup message.
