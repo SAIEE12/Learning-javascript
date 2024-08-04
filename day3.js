92935ed601f273a2255fc2d28b49d2d85c2711f5
Your Name <your.email@example.com>
2024-08-04 16:23:14 +0530

    day3 second commit

--- day3.js
+++ day3.js
@@ -2,15 +2,46 @@
 // b = "3";
 // console.log(a !== b);
 
-let mode = "dark";
-let color;
+// let mode = "dark";
+// let color;
 
-if (mode === "dark"){
-    color = "black"
-}
-if(mode === "light"){
-    color = "White"
-}
-console.log(color);
+// if (mode === "dark"){
+//     color = "black"
+// }
+// if(mode === "light"){
+//     color = "White"
+// }
+// console.log(color);
 
-alert("welcome to my world..."); //one tme popup message.
+// alert("welcome to my world..."); //one tme popup message.
+// prompt("enter the number");//its is similar to the alert
+                           // and takes input from the user
+
+//prac_ques-1 : take the num through from prompt and check it is multiple with 5
+
+// let num = prompt("Enter the number");
+
+// if (num % 5 === 0){
+//     console.log(num,"is multiple of five");
+// }
+// else{
+//     console.log(num,"is not a multiple of five");
+// }
+//prac_ques-2 : student score 
+
+let score = prompt("enter your score(0-100):");
+let grade;
+if (score >= 90 && score <=100){
+    grade =  "A";
+}
+else if (score >=70 && score <=89){
+    grade = "B"
+}
+else if (score >=50 && score <=69){
+    grade = "C"
+}
+else{
+    grade = "D"
+}
+console.log(grade);
+
